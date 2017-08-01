4e6ca0048665a1c96728e49b5a270b94440aba05
Joe Donghee Cho <recordingbetter@gmail.com>
2017-08-01 20:07:45 +0900

    modify: user admin

--- django_app/member/models/pickyuser.py
+++ django_app/member/models/pickyuser.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
-from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 __all__ = (
@@ -8,8 +7,8 @@ __all__ = (
 
 
 class PickyUserManager(BaseUserManager):
-    # create Manager
-    def create_user(self, email, nickname, password=None, profile_image=None, profile_content=None):
+    # create Manager : 작업하면서 내용 확인 필요 - 8/1 Joe
+    def create_user(self, email, nickname, password=None, img_profile=None, content=None):
         if not email:
             raise ValueError('email을 입력하세요.')
 
@@ -21,7 +20,7 @@ class PickyUserManager(BaseUserManager):
         user.save()
         return user
 
-    # createsuperuser manager : 디버그모드에서 확인완료 - Joe
+    # createsuperuser manager : 디버그모드에서 확인완료 - 8/1 Joe
     def create_superuser(self, email, nickname, password):
         user = self.create_user(
                 email=email,
@@ -34,36 +33,48 @@ class PickyUserManager(BaseUserManager):
 
 
 class PickyUser(AbstractBaseUser):
+    # user id
     email = models.EmailField(
             verbose_name='email',
             max_length=100,
             unique=True
     )
+    # user nickname
     nickname = models.CharField(
             max_length=100,
             unique=True
     )
+    # user 사진
+    img_profile = models.ImageField(
+            upload_to='media/user',
+            blank=True,
+            null=True
+    )
+    # user 인삿말
+    content = models.TextField(blank=True)
     # django user : d
     # facebook user : f
     # naver user : n
     # kakao user : k
     id_type = models.CharField(default='d', max_length=1)
+    # 활성화된 유저인가? admin 페이지때문에 필수 : 8/1 Joe
     is_active = models.BooleanField(default=True)
+    # 관리자인가? admin 페이지때문에 필수 : 8/1 Joe
     is_admin = models.BooleanField(default=False)
-    # is_staff = models.BooleanField(default=False)
 
     objects = PickyUserManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = [
         'nickname',
-        # 'id_type'
     ]
 
+    # admin 페이지때문에 필수 : 8/1 Joe
     def get_full_name(self):
         # The user is identified by their email address
         return self.email
 
+    # admin 페이지때문에 필수 : 8/1 Joe
     def get_short_name(self):
         # The user is identified by their email address
         return self.email
@@ -71,31 +82,15 @@ class PickyUser(AbstractBaseUser):
     def __str__(self):
         return self.email
 
+    # admin 페이지때문에 필수 : 8/1 Joe
     def has_perm(self, perm, obj=None):
         return True
 
+    # admin 페이지때문에 필수 : 8/1 Joe
     def has_module_perms(self, app_label):
         return True
 
+    # admin 페이지때문에 필수 : 8/1 Joe
     @property
     def is_staff(self):
         return self.is_admin
-
-# class PickyUser(AbstractUser):
-#     # 프로필 사진
-#     profile_image = models.ImageField()
-#     # 프로필 설명
-#     profile_content = models.TextField(max_length=256)
-#     # 타 사이트 아이디1
-#     sns_username = models.EmailField(max_length=50)
-#     # 타 사이트 아이디2
-#     # another_id2 = models.CharField(max_length=50)
-#     # 닉네임
-#     nickname = models.CharField(max_length=30)
-#     # 아이디 타입 (로그인시 구분)
-#     id_type = models.CharField(max_length=30)
-
-
-
-
-
