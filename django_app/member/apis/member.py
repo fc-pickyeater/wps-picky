513cd6a258cf63de3336898b802f86ba4a1eda7d
Joe Donghee Cho <recordingbetter@gmail.com>
2017-08-03 21:35:42 +0900

    add: PickyUser Detail, Create API views

--- django_app/member/apis/member.py
+++ django_app/member/apis/member.py
@@ -5,10 +5,22 @@ from ..models import PickyUser
 
 __all__ = (
     'PickyUserList',
+    'PickyUserDetail',
+    'PickyUserCreate',
 )
 
 
 # user list / test 용도 8/2 Joe
 class PickyUserList(generics.ListAPIView):
+    queryset = PickyUser.objects.all()
+    serializer_class = PickyUserSerializer
+
+
+class PickyUserDetail(generics.RetrieveAPIView):
+    queryset = PickyUser.objects.all()
+    serializer_class = PickyUserSerializer
+
+
+class PickyUserCreate(generics.CreateAPIView):
     queryset = PickyUser.objects.all()
     serializer_class = PickyUserSerializer
--- django_app/member/urls.py
+++ django_app/member/urls.py
@@ -4,4 +4,6 @@ from . import apis
 
 urlpatterns = [
     url(r'^$', apis.PickyUserList.as_view()),
+    url(r'(?P<pk>\d+)/$', apis.PickyUserDetail.as_view()),
+    url(r'^create/$', apis.PickyUserCreate.as_view()),
 ]
