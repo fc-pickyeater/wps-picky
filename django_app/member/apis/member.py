400ade5e31b4684c53fdc22538a809477eddf251
Joe Donghee Cho <recordingbetter@gmail.com>
2017-08-04 23:21:01 +0900

    modify: member serializer import path

--- django_app/member/apis/member.py
+++ django_app/member/apis/member.py
@@ -1,6 +1,10 @@
 from rest_framework import generics
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import IsAuthenticated
 
-from ..serializers import PickyUserSerializer, PickyUserCreateSerializer, PickyUserDetailSerializer
+from member.serializers.other import PickyUserSerializer, PickyUserDetailSerializer
+from member.serializers.signup import PickyUserCreateSerializer
 from ..models import PickyUser
 
 __all__ = (
@@ -12,16 +16,19 @@ __all__ = (
 
 
 # user list / test 용도 8/2 Joe
+# postman, 배포환경에서 정상작동 확인 8/4 Joe
 class PickyUserList(generics.ListAPIView):
     queryset = PickyUser.objects.all()
     serializer_class = PickyUserSerializer
 
 
+# postman, 배포환경에서 GET만 정상작동 확인 8/4 Joe
 class PickyUserDetailUpdate(generics.RetrieveUpdateAPIView):
     queryset = PickyUser.objects.all()
     serializer_class = PickyUserDetailSerializer
 
 
+# postman, 배포환경에서 확인함. 데이터 생성은 되지만 password 저장되지않음 8/4 Joe
 class PickyUserCreate(generics.CreateAPIView):
     queryset = PickyUser.objects.all()
     serializer_class = PickyUserCreateSerializer
@@ -30,3 +37,13 @@ class PickyUserCreate(generics.CreateAPIView):
 class PickyUserDelete(generics.DestroyAPIView):
     queryset = PickyUser.objects.all()
     serializer_class = PickyUserSerializer
+
+
+# 작업중 8/4 joe
+class PickyUserLogin(generics.GenericAPIView):
+    authentication_classes = (SessionAuthentication, BasicAuthentication)
+    permission_classes = (IsAuthenticated,)
+
+    # def get
+
+
