26b5ec7cec1e576cb0f89b0166b5342b4f0fe508
Joe Donghee Cho <recordingbetter@gmail.com>
2017-08-07 12:05:51 +0900

    add: rest_framework Token Auth package

--- django_app/config/settings/base.py
+++ django_app/config/settings/base.py
@@ -56,7 +56,13 @@ INSTALLED_APPS = [
 ]
 # 8/1 hong 추가 search filter html보여주는듯? -hong 8/1
 REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',)
+    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    )
 }
 
 MIDDLEWARE = [
