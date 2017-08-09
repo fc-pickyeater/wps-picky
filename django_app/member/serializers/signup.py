8af0f00147fe0b58d967d299bbc792cd5f424274
Joe Donghee Cho <recordingbetter@gmail.com>
2017-08-09 20:59:40 +0900

    add: email validation to user create serializer

--- django_app/member/serializers/signup.py
+++ django_app/member/serializers/signup.py
@@ -1,5 +1,7 @@
 # 배포 후 이미지를 저장하기 위해 조사한 예시 8/4 Joe
 # 배포후 테스트필요. 로컬에서 작동됨. 8/4 Joe
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 
@@ -35,6 +37,8 @@ class PickyUserCreateSerializer(serializers.Serializer):
     def validate_email(self, email):
         if PickyUser.objects.filter(email=email).exists():
             raise serializers.ValidationError('다른 사용자가 사용 중인 email입니다.')
+        elif validate_email(email):
+            raise serializers.ValidationError('유효한 이메일 주소를 입력하십시오.')
         return email
 
     def validate_nickname(self, nickname):
