579b4621fea66bed32d0227577eeb7c928125abc
seung8 <lsy0914@naver.com>
2017-08-15 10:44:49 +0900

    Delete RecipeReviewListAPI

--- django_app/recipe/apis/recipe_review.py
+++ django_app/recipe/apis/recipe_review.py
@@ -3,7 +3,7 @@ from rest_framework import permissions
 from rest_framework.permissions import IsAuthenticated
 
 from utils.permissions import ObjectIsRequestUser
-from ..serializers import RecipeReviewListSerializer, RecipeReviewModifySerializer, RecipeReviewCreateSerializer
+from ..serializers import RecipeReviewModifySerializer, RecipeReviewCreateSerializer
 from ..models import RecipeReview, Recipe
 
 
