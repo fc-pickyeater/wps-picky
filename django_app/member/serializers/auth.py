cfbae4603b27d622a993f4bf195dd6535a0e468b
Joe Donghee Cho <recordingbetter@gmail.com>
2017-08-16 23:45:07 +0900

    sign up, log in 에러 메세지 출력 방법 수정

--- django_app/member/serializers/auth.py
+++ django_app/member/serializers/auth.py
@@ -1,8 +1,9 @@
 from django.contrib.auth import authenticate
 
-from rest_framework import serializers
+from rest_framework import serializers, status
 from rest_framework.authtoken.models import Token
 
+from utils.exceptions import CustomValidationError
 
 __all__ = (
     'PickyAuthTokenSerializer',
@@ -20,26 +21,34 @@ class PickyUserTokenSerializer(serializers.ModelSerializer):
 
 
 class PickyAuthTokenSerializer(serializers.Serializer):
-    email = serializers.CharField()
-    password = serializers.CharField(style={'input_type': 'password'})
+    email = serializers.CharField(allow_null=True, required=False)
+    password = serializers.CharField(allow_null=True, required=False, style={'input_type': 'password'})
 
     def validate(self, attrs):
-        email = attrs.get('email')
-        password = attrs.get('password')
+        d = dict()
+        email = attrs.get('email', None)
+        password = attrs.get('password', None)
 
         if email and password:
             user = authenticate(email=email, password=password)
 
             if user:
                 if not user.is_active:
-                    msg = 'User account is disabled.'
-                    raise serializers.ValidationError(msg, code='authorization')
+                    d['activation_error'] = '활성화되지 않은 계정입니다.'
+                    raise CustomValidationError(d, code='authorization')
             else:
-                msg = 'email 또는 비밀번호가 맞지 않습니다.'
-                raise serializers.ValidationError(msg, code='authorization')
-        else:
-            msg = 'Must include "email" and "password".'
-            raise serializers.ValidationError(msg, code='authorization')
+                d['login_error'] = 'email 또는 비밀번호가 맞지 않습니다.'
+                # msg = 'email 또는 비밀번호가 맞지 않습니다.'
+                raise CustomValidationError(d, code='authorization')
+        elif (not email and not password) or (password is None and email is None):
+            d['empty_error'] = 'email과 password를 입력해주세요.'
+            raise CustomValidationError(d, code='authorization')
+        elif (email and not password) or password is None:
+            d['empty_password'] = 'password를 입력해주세요.'
+            raise CustomValidationError(d, code='authorization')
+        elif (not email and password) or email in None:
+            d['empty_email'] = 'email을 입력해주세요.'
+            raise CustomValidationError(d, code='authorization')
 
         attrs['user'] = user
         return attrs
