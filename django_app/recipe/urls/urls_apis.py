96e4d06622a10c9320c729b90e272a6a4ec2d543
Joe Donghee Cho <recordingbetter@gmail.com>
2017-08-20 13:58:55 +0900

    modify recipe step ordering, serializers

--- django_app/recipe/urls/urls_apis.py
+++ django_app/recipe/urls/urls_apis.py
@@ -12,12 +12,12 @@ urlpatterns = [
     # recipe step
     # url(r'^step/$', apis.RecipeStepCreateView.as_view()),
     # recipestepcomment
-    url(r'^step/comment/$', apis.RecipeStepCommentListView.as_view()),
-    url(r'^step/(?P<pk>\d+)/comment/create/$', apis.RecipeStepCommentCreateView.as_view()),
-    url(r'^step/comment/modify/(?P<pk>\d+)/$', apis.RecipeStepCommentModifyView.as_view()),  # 수정삭제
-    url(r'^step/(?P<pk>\d+)/$', apis.RecipeStepModifyDeleteView.as_view()),
+    # url(r'^step/comment/$', apis.RecipeStepCommentListView.as_view()),
+    # url(r'^step/(?P<pk>\d+)/comment/create/$', apis.RecipeStepCommentCreateView.as_view()),
+    # url(r'^step/comment/modify/(?P<pk>\d+)/$', apis.RecipeStepCommentModifyView.as_view()),  # 수정삭제
+    # url(r'^step/(?P<pk>\d+)/$', apis.RecipeStepModifyDeleteView.as_view()),
     # recipe search
-    url(r'^search/$', apis.RecipeSearchListView.as_view()),
+    # url(r'^search/$', apis.RecipeSearchListView.as_view()),
 
     # 레시피 생성
     url(r'^create/$', apis.RecipeCreateForFDS.as_view()),
