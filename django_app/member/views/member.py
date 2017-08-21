e7c47a82ea6955034afd54185240248cc5f53c0a
Joe Donghee Cho <recordingbetter@gmail.com>
2017-08-21 15:16:28 +0900

    recipe list html 분리

--- django_app/member/views/member.py
+++ django_app/member/views/member.py
@@ -1,5 +1,8 @@
 from django.contrib.auth import get_user_model
 from django.views.generic import DetailView, ListView
+from django.views.generic.detail import SingleObjectMixin
+
+from recipe.models import Recipe
 
 PickyUser = get_user_model()
 
@@ -11,6 +14,17 @@ __all__ = (
 
 class PickyUserDetailView(DetailView):
     model = PickyUser
+
+    def get_context_data(self, **kwargs):
+        context = {}
+        if self.object:
+            context['object'] = self.object
+            context['recipe_list'] = Recipe.objects.filter(user=self.object)
+            context_object_name = self.get_context_object_name(self.object)
+            if context_object_name:
+                context[context_object_name] = self.object
+        context.update(kwargs)
+        return super(SingleObjectMixin, self).get_context_data(**context)
 
 
 class PickyUserListView(ListView):
