1aa783a78305809781eca62b7dc9e249e6220b16
Joe Donghee Cho <recordingbetter@gmail.com>
2017-08-21 15:00:11 +0900

    add: recipe search template, view, form

--- django_app/recipe/views/index_search.py
+++ django_app/recipe/views/index_search.py
@@ -1,3 +1,35 @@
+from django.db.models import Q
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+
+from recipe.forms.recipe_search import RecipeSearchForm
+from recipe.models import Recipe
+
 
 def recipe_search(request):
-    pass
+    if request.method == 'GET':
+        form = RecipeSearchForm()
+        print(form)
+        context = {
+            'form': form,
+        }
+        return render(request, 'index.html', context=context)
+
+    elif request.method == 'POST':
+        form = RecipeSearchForm(request.POST)
+        search = request.POST.get('search')
+        if form.is_valid():
+            recipe_id = list()
+            search_result = Recipe.objects.filter(
+                    Q(title__contains=search)|
+                    Q(ingredient__contains=search)|
+                    Q(recipetag__tag__content__contains=search)
+            )
+            if search_result:
+                for recipe in search_result:
+                    recipe_id.append(recipe.id)
+                result = Recipe.objects.filter(pk__in=recipe_id)
+                context = {
+                    'recipe_list': result,
+                }
+        return render(request, 'recipe/recipe_list.html', context)
