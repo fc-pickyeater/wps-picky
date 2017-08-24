ca8861a1f1530328db77760b87dcb3ab41b87f88
seung8 <lsy0914@naver.com>
2017-08-24 12:42:58 +0900

    edit wsgi/debug.py, deploy.py : add comments

--- django_app/config/wsgi/debug.py
+++ django_app/config/wsgi/debug.py
@@ -2,6 +2,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+# WSGI 구동 환경 기본값 지정 (debug 모드
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.debug")
 
+# WSGI 사용
 application = get_wsgi_application()
--- django_app/config/wsgi/deploy.py
+++ django_app/config/wsgi/deploy.py
@@ -2,6 +2,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+# WSGI 구동 환경 기본값 지정 (deploy 모드)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.deploy")
 
+# WSGI 사용
 application = get_wsgi_application()
