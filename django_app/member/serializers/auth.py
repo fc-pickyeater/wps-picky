c32529325cd02ab062715581c7d0c07399c40b7d
Joe Donghee Cho <recordingbetter@gmail.com>
2017-08-24 11:59:33 +0900

    modify user login, signup error points

--- django_app/member/serializers/auth.py
+++ django_app/member/serializers/auth.py
@@ -29,10 +29,19 @@ class PickyAuthTokenSerializer(serializers.Serializer):
         d = dict()
         email = attrs.get('email', None)
         password = attrs.get('password', None)
-
-        if email and password:
+        # email, password가 비어있는 경우를 걸러냄
+        if (email == '' and password == '') or (password is None and email is None):
+            d['empty_error'] = 'email과 password를 입력해주세요.'
+            raise CustomValidationError(d, code='authorization')
+        elif (email and password == '') or password is None:
+            d['empty_password'] = 'password를 입력해주세요.'
+            raise CustomValidationError(d, code='authorization')
+        elif (email == '' and password) or email is None:
+            d['empty_email'] = 'email을 입력해주세요.'
+            raise CustomValidationError(d, code='authorization')
+        # email, password가 모두 입력되었을 경우
+        else:
             user = authenticate(email=email, password=password)
-
             if user:
                 if not user.is_active:
                     d['activation_error'] = '활성화되지 않은 계정입니다.'
@@ -40,14 +49,6 @@ class PickyAuthTokenSerializer(serializers.Serializer):
             else:
                 d['login_error'] = 'email 또는 비밀번호가 맞지 않습니다.'
                 raise CustomValidationError(d, code='authorization')
-        elif (not email and not password) or (password is None and email is None):
-            d['empty_error'] = 'email과 password를 입력해주세요.'
-            raise CustomValidationError(d, code='authorization')
-        elif (email and not password) or password is None:
-            d['empty_password'] = 'password를 입력해주세요.'
-            raise CustomValidationError(d, code='authorization')
-        elif (not email and password) or email is None:
-            d['empty_email'] = 'email을 입력해주세요.'
-            raise CustomValidationError(d, code='authorization')
+
         attrs['user'] = user
         return attrs
