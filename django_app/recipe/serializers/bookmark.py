c98133148b0f27d5a772ec8f23e40b3b0eb20b81
Joe Donghee Cho <recordingbetter@gmail.com>
2017-09-09 03:41:15 +0900

    modify bookmark

--- django_app/recipe/serializers/bookmark.py
+++ django_app/recipe/serializers/bookmark.py
@@ -31,10 +31,11 @@ class BookMarkSerializer(serializers.ModelSerializer):
         ret['rate_sum'] = recipe.rate_sum
         ret['like_count'] = recipe.like_count
         # img_recipe의 값이 없을 경우 에러 발생함. 9/6 Joe
-        try:
-            img_path = recipe.img_recipe.path
-        except ValueError:
-            ret['img_recipe'] = None
-        else:
-            ret['img_recipe'] = img_path
+        ret['img_recipe'] = recipe.img_recipe.path
+        # try:
+        #     img_path = recipe.img_recipe.path
+        # except ValueError:
+        #     ret['img_recipe'] = None
+        # else:
+        #     ret['img_recipe'] = img_path
         return ret
