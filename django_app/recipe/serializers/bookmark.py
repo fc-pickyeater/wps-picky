482c03fb62e31e94b4ea293c66a4834d6dec0a6a
Joe Donghee Cho <recordingbetter@gmail.com>
2017-09-09 04:26:57 +0900

    modify bookmark

--- django_app/recipe/serializers/bookmark.py
+++ django_app/recipe/serializers/bookmark.py
@@ -1,15 +1,22 @@
 from rest_framework import serializers
 
 from recipe.models import BookMark, Recipe
+from recipe.serializers.recipe import RecipeListSerializer
 
 
 class BookMarkSerializer(serializers.ModelSerializer):
+    # img_recipe = RecipeListSerializer(many=True)
+    # img_recipe = serializers.ImageField(allow_null=True, required=False)
+    img_recipe = serializers.ImageField(default=Recipe.img_recipe)
+
     class Meta:
+
         model = BookMark
         fields = (
             'pk',
             'user',
             'recipe',
+            'img_recipe',
             'memo',
             'created_date',
 
@@ -18,6 +25,7 @@ class BookMarkSerializer(serializers.ModelSerializer):
         read_only_fields = (
             'user',
             'recipe',
+            'img_recipe',
             'created_date'
         )
 
@@ -31,7 +39,7 @@ class BookMarkSerializer(serializers.ModelSerializer):
         ret['rate_sum'] = recipe.rate_sum
         ret['like_count'] = recipe.like_count
         # img_recipe의 값이 없을 경우 에러 발생함. 9/6 Joe
-        ret['img_recipe'] = recipe.img_recipe.path
+        # ret['img_recipe'] = recipe.img_recipe
         # try:
         #     img_path = recipe.img_recipe.path
         # except ValueError:
